54233d8e299de90099f8190bc8f86e61d31f6fc6
XShubham07 <xshubham7@protonmail.com>
2025-11-25 23:47:26 +0530

    update

--- src/components/PackageGrid.jsx
+++ src/components/PackageGrid.jsx
@@ -3,12 +3,10 @@ import PackageCard from "./PackageCard";
 
 export default function PackageGrid({ list, onView }) {
   const [hoverIndex, setHoverIndex] = useState(null);
-
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
       {list.map((p, i) => {
         const isHovered = hoverIndex === i;
-
         return (
           <div
             key={i}
