61a60460f35d0a5c3055b73f8e6e63c2905e9f06
Shubham Kumar <xshubham7@protonmail.com>
2025-12-16 12:58:00 +0530

    Update App.jsx with blog routes and add required dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Home from "./pages/Home";
 import Tours from "./pages/Tours";
 import TourDetailsPage from "./pages/TourDetailsPage";
 import Blog from "./pages/Blog";
+import BlogPost from "./pages/BlogPost";
 import Contact from "./pages/Contact";
 import Login from "./pages/Login";
 import AgentDashboard from "./pages/AgentDashboard";
@@ -15,7 +16,7 @@ import BookingStatus from "./pages/BookingStatus";
 import AllReviews from "./pages/AllReviews";
 import Destinations from "./pages/Destinations";
 import About from "./pages/About";
-import Terms from "./pages/Terms"; // 
+import Terms from "./pages/Terms";
 
 export default function App() {
   return (
@@ -26,11 +27,15 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/terms" element={<Terms />} /> {/* <--- 2. Add Route */}
+          <Route path="/terms" element={<Terms />} />
           <Route path="/tours" element={<Tours />} />
           <Route path="/tours/:id" element={<TourDetailsPage />} />
           <Route path="/destinations" element={<Destinations />} />
+          
+          {/* Blog Routes */}
           <Route path="/blog" element={<Blog />} />
+          <Route path="/blog/:slug" element={<BlogPost />} />
+          
           <Route path="/contact" element={<Contact />} />
           <Route path="/status" element={<BookingStatus />} />
           <Route path="/reviews" element={<AllReviews />} />
