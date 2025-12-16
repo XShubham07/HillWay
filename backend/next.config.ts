7e8d910576d4ad9d23bc408cf7ca67fceb904ab0
Shubham <xshubham7@Protonmail.com>
2025-12-16 13:02:42 +0530

    Remove old Next.js blog and backend folders

--- backend/next.config.ts
+++
@@ -1,7 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  // We removed the headers() function because CORS is now handled in middleware.js
-};
-
-export default nextConfig;
